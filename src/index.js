600d310459a0f7cb1e911e8b70b1a6a91e182d96
David Konsumer <konsumer@jetboystudio.com>
2017-09-26 11:47:45 -0700

    added stringMode option

--- src/index.js
+++ src/index.js
@@ -6,57 +6,6 @@ const indent = count => Array(count).join('  ')
 // is a number a float?
 const isFloat = n => Number(n) === n && n % 1 !== 0
 
-/**
- * Turn a protobuf into a data-object
- *
- * @param      {Buffer}   buffer  The proto in a binary buffer
- * @return     {object[]}         Info about the protobuf
- */
-export const getData = buffer => {
-  const reader = Reader.create(buffer)
-  const out = []
-  while (reader.pos < reader.len) {
-    const tag = reader.uint64()
-    const id = tag >>> 3
-    const wireType = tag & 7
-    switch (wireType) {
-      case 0: // int32, int64, uint32, bool, enum, etc
-        out.push({[id]: reader.uint32()})
-        break
-      case 1: // fixed64, sfixed64, double
-        out.push({[id]: reader.fixed64()})
-        break
-      case 2: // string, bytes, sub-message
-        const bytes = reader.bytes()
-        try {
-          const innerMessage = getData(bytes)
-          out.push({[id]: innerMessage})
-        } catch (e) {
-          // search buffer for extended chars
-          let hasExtended = false
-          bytes.forEach(b => {
-            if (b < 32) {
-              hasExtended = true
-            }
-          })
-          if (hasExtended) {
-            out.push({[id]: bytes})
-          } else {
-            out.push({[id]: bytes.toString()})
-          }
-        }
-        break
-      // IGNORE start_group
-      // IGNORE end_group
-      case 5: // fixed32, sfixed32, float
-        out.push({[id]: reader.float()})
-        break
-      default: reader.skipType(wireType)
-    }
-  }
-  return out
-}
-
 // turn a message into a proto-representation
 const handleMessage = (msg, m = 'Root', level = 1) => {
   const seen = []
@@ -66,8 +15,9 @@ const handleMessage = (msg, m = 'Root', level = 1) => {
     const t = Array.isArray(field[n]) ? 'array' : typeof field[n]
     switch (t) {
       case 'object': // it's a buffer
+        return `${indent(level + 1)}bytes field${n} = ${n}; // could be a repeated-value, string, bytes, or malformed sub-message`
       case 'string':
-        return `${indent(level + 1)}string field${n} = ${n}; // could be a repeated-value, string, buffer, or malformed sub-message`
+        return `${indent(level + 1)}string field${n} = ${n}; // could be a repeated-value, string, bytes, or malformed sub-message`
       case 'number':
         return isFloat(field[n])
           ? `${indent(level + 1)}float field${n} = ${n}; // could be a fixed64, sfixed64, double, fixed32, sfixed32, or float`
@@ -96,13 +46,72 @@ const handleMessage = (msg, m = 'Root', level = 1) => {
 }
 
 /**
+ * Turn a protobuf into a data-object
+ *
+ * @param      {Buffer}   buffer     The proto in a binary buffer
+ * @param      {string}   stringMode How to handle strings that aren't sub-messages: "auto" - guess based on chars, "string" - always a string, "binary" - always a buffer
+ * @return     {object[]}            Info about the protobuf
+ */
+export const getData = (buffer, stringMode = 'auto') => {
+  const reader = Reader.create(buffer)
+  const out = []
+  while (reader.pos < reader.len) {
+    const tag = reader.uint64()
+    const id = tag >>> 3
+    const wireType = tag & 7
+    switch (wireType) {
+      case 0: // int32, int64, uint32, bool, enum, etc
+        out.push({[id]: reader.uint32()})
+        break
+      case 1: // fixed64, sfixed64, double
+        out.push({[id]: reader.fixed64()})
+        break
+      case 2: // string, bytes, sub-message
+        const bytes = reader.bytes()
+        try {
+          const innerMessage = getData(bytes, stringMode)
+          out.push({[id]: innerMessage})
+        } catch (e) {
+          if (stringMode === 'binary') {
+            out.push({[id]: bytes})
+          } else if (stringMode === 'string') {
+            out.push({[id]: bytes.toString()})
+          } else {
+            // search buffer for extended chars
+            let hasExtended = false
+            bytes.forEach(b => {
+              if (b < 32) {
+                hasExtended = true
+              }
+            })
+            if (hasExtended) {
+              out.push({[id]: bytes})
+            } else {
+              out.push({[id]: bytes.toString()})
+            }
+          }
+        }
+        break
+      // IGNORE start_group
+      // IGNORE end_group
+      case 5: // fixed32, sfixed32, float
+        out.push({[id]: reader.float()})
+        break
+      default: reader.skipType(wireType)
+    }
+  }
+  return out
+}
+
+/**
  * Gets the proto-definition string from a binary protobuf message
  *
  * @param      {Buffer}  buffer  The buffer
+ * @param      {string}   stringMode How to handle strings that aren't sub-messages: "auto" - guess based on chars, "string" - always a string, "binary" - always a buffer
  * @return     {string}  The proto
  */
-export const getProto = buffer => {
-  const data = getData(buffer)
+export const getProto = (buffer, stringMode = 'auto') => {
+  const data = getData(buffer, stringMode)
   let out = 'syntax = "proto3";\n\n'
   out += handleMessage(data)
   return out
--- test/test.js
+++ test/test.js
@@ -11,5 +11,11 @@ console.log(hexy(encoded))
 console.log('Structure:')
 console.log(JSON.stringify(getData(encoded), null, 2), '\n')
 
+console.log('Structure (all binary):')
+console.log(JSON.stringify(getData(encoded, 'binary'), null, 2), '\n')
+
 console.log('Proto:')
 console.log(getProto(encoded))
+
+console.log('Proto (all binary):')
+console.log(getProto(encoded, 'binary'))
