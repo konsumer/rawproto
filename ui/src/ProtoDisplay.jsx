003e9ffafa50cdf25e080cebde73a604b9289afb
David Konsumer <konsumer@jetboystudio.com>
2024-05-02 23:07:52 -0700

    linkify

--- ui/src/ProtoDisplay.jsx
+++ ui/src/ProtoDisplay.jsx
@@ -1,5 +1,6 @@
 import RawProto, { decoders, parseLabels, wireLabels, wireMap, wireTypes } from 'rawproto'
 import { useEffect, useState } from 'react'
+import Linkify from 'react-linkify'
 
 const badgeColors = {}
 badgeColors[wireTypes.VARINT] = 'default'
@@ -37,7 +38,7 @@ function ProtoField(field) {
   return (
     <div>
       <div className={`badge badge-${badgeColors[field.type]} gap-2`}>{field.index}</div>
-      {decoders.display(field)}
+      <Linkify properties={{ target: '_new', className: 'underline' }}>{decoders.display(field)}</Linkify>
     </div>
   )
 }
