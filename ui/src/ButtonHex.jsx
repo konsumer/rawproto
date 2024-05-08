27cadbda6077fd7cd7960d3ce4c40730d88fbce8
David Konsumer <konsumer@jetboystudio.com>
2024-05-08 12:06:12 -0700

    string-input modal and better hex function #15

---
+++ ui/src/ButtonHex.jsx
@@ -0,0 +1,33 @@
+import { useRef, useState } from 'react'
+
+const fromHexString = (hexString) => {
+  const b = Uint8Array.from(hexString.split(' ').filter(e => e).map((byte) => parseInt(byte, 16)))
+  console.log(b)
+  return b
+}
+
+export default function ButtonHex ({ className = 'btn btn-neutral', children = 'STRING', onChange }) {
+  const [value, setValue] = useState('')
+  const r = useRef()
+
+  const handleButtonClick = () => r.current.showModal()
+  const handleOkClick = () => onChange(fromHexString(value))
+  const handleHexChange = e => setValue(e.target.value)
+
+  return (
+    <>
+      <button onClick={handleButtonClick} className={className}>{children}</button>
+      <dialog ref={r} className='modal'>
+        <div className='modal-box'>
+          <h3 className='font-bold text-lg'>String Byte Input</h3>
+          <p className='py-4'>Input your hex-string here.</p>
+          <textarea placeholder='08 96 01 0a ..' className='textarea textarea-bordered textarea-lg w-full' value={value} onChange={handleHexChange} />
+          <form method='dialog' className='modal-action gap-2'>
+            <button className='btn'>Close</button>
+            <button className='btn btn-primary' onClick={handleOkClick}>Ok</button>
+          </form>
+        </div>
+      </dialog>
+    </>
+  )
+}
