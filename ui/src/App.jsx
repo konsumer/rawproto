149c60ade622913b3b41dc0d2a910d1e44b0fc13
David Konsumer <konsumer@jetboystudio.com>
2024-05-10 02:30:13 -0700

    moar tests, starting work on ui

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,25 +1,29 @@
 import { useState } from 'react'
+import RawProtoMessage from 'rawproto/react'
+import ErrorBoundary from './ErrorBoundary.jsx'
 import Reader from 'rawproto'
 
-import ProtoDisplay from './ProtoDisplay.jsx'
-import ErrorBoundary from './ErrorBoundary.jsx'
-import ButtonHex from './ButtonHex.jsx'
+const isHex = (maybeHex) =>
+  maybeHex.length !== 0 && maybeHex.length % 2 === 0 && !/[^a-fA-F0-9]/u.test(maybeHex)
+
+const fromHexString = (hexString) =>
+  Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)))
 
 function App () {
-  const [fields, setFields] = useState([])
+  const [root, setRoot] = useState([])
 
   const handleFileChange = async (e) => {
     const file = e.target.files[0]
     if (file) {
-      const tree = new Reader(new Uint8Array(await file.arrayBuffer())).readMessage()
-      // console.log(JSON.stringify(tree, null, 2))
-      setFields(tree)
+      setRoot(new Reader(new Uint8Array(await file.arrayBuffer())))
     }
   }
 
-  const handleHexChange = bytes => {
-    const tree = new Reader(bytes).readMessage()
-    setFields(tree)
+  const handleHexChange = async (e) => {
+    const input = e.target.value.replaceAll(' ', '')
+    if (isHex(input)) {
+      setRoot(new Reader(fromHexString(input)))
+    }
   }
 
   return (
@@ -28,16 +32,11 @@ function App () {
         <h1 className='text-4xl text-accent grow'>RawProto</h1>
       </header>
       <main className='p-4 grow overflow-y-auto'>
-        {!fields?.length && <p className='mb-2'>Choose a binary protobuf file, or input a string, and you can explore it. No data is sent to any server (all local.)</p>}
-        <div className='flex gap-2'>
-          <ButtonHex onChange={handleHexChange} />
-          <div>
-            <input type='file' className='mb-2 w-full file-input' onChange={handleFileChange} />
-          </div>
-        </div>
-
+        {!root?.length && <p className='mb-2'>Choose a binary protobuf file, and you can explore it. No data is sent to any server (all local.)</p>}
+        <input type='file' className='mb-2 w-full file-input' onChange={handleFileChange} />
+        <input type='text' className='mb-2 w-full border-2 px-5' placeholder='Hex string input' onChange={handleHexChange} />
         <ErrorBoundary>
-          <ProtoDisplay className='w-full menu bg-base-200 rounded-box' fields={fields} />
+          {!!root?.length && (<RawProtoMessage message={root} />)}
         </ErrorBoundary>
       </main>
       <footer className='p-5 text-center bg-neutral text-neutral-content'>
